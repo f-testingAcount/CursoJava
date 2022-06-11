92235c2adbd1b3d7ce221f75ec52da8fea534f56
f-testingAcount <fdchb2@gmail.com>
2022-06-10 21:36:26 -0300

    Programacion Operacion Excepcion bloque finally

--- Programacion/ManejoExcepciones 1/src/com/udemy/Ejecutable.java
+++ Programacion/ManejoExcepciones 1/src/com/udemy/Ejecutable.java
@@ -5,6 +5,7 @@
 package com.udemy;
 
 import static com.udemy.aritmentica.Aritmetica.division;
+import com.udemy.excepciones.OperacionExcepcion;
 
 /**
  *
@@ -17,13 +18,21 @@ public class Ejecutable {
      */
     public static void main(String[] args) {
         int resultado = 0;
-        //try {  //En el caso del RuntimeException las lineas comentadas no son necesarias y al manifestarce una excepcion el programa termina de una y marca la excepcion en la consola. 
+        try {  //En el caso del RuntimeException las lineas comentadas no son necesarias y al manifestarce una excepcion el programa termina de una y marca la excepcion en la consola. 
             resultado = division(10, 0);
-//        } catch (Exception e) {
-//            System.out.println("Detalle del error: ");
-//            e.printStackTrace(System.out);
-//            System.out.println(e.getMessage());
-//        }
+        } catch(OperacionExcepcion e){
+            System.out.println("Ocurrio un error de tipo OperacionExcepcion");
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Detalle del error: ");
+            e.printStackTrace(System.out);
+            System.out.println(e.getMessage());
+        }
+        finally { //Este bloque se ejecuta siempre independientemente de si ocurre una excepcion o no.
+                  //Si tenemos un bloque try y catch el bloque finally se ejecuta siempre.
+            System.out.println("Se reviso la divion por cero");
+        }
+        
         System.out.println("resultado = " + resultado);
     }
 
