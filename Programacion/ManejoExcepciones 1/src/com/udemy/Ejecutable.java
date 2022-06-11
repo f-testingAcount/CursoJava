63bab76a43ed46a90a1de164aada31711db33d3b
f-testingAcount <fdchb2@gmail.com>
2022-06-10 21:27:41 -0300

    Programacion Excepciones RuntimeException

--- Programacion/ManejoExcepciones 1/src/com/udemy/Ejecutable.java
+++ Programacion/ManejoExcepciones 1/src/com/udemy/Ejecutable.java
@@ -17,13 +17,13 @@ public class Ejecutable {
      */
     public static void main(String[] args) {
         int resultado = 0;
-        try {
+        //try {  //En el caso del RuntimeException las lineas comentadas no son necesarias y al manifestarce una excepcion el programa termina de una y marca la excepcion en la consola. 
             resultado = division(10, 0);
-        } catch (Exception e) {
-            System.out.println("Detalle del error: ");
-            e.printStackTrace(System.out);
-            System.out.println(e.getMessage());
-        }
+//        } catch (Exception e) {
+//            System.out.println("Detalle del error: ");
+//            e.printStackTrace(System.out);
+//            System.out.println(e.getMessage());
+//        }
         System.out.println("resultado = " + resultado);
     }
 
--- Programacion/ManejoExcepciones 1/src/com/udemy/aritmentica/Aritmetica.java
+++ Programacion/ManejoExcepciones 1/src/com/udemy/aritmentica/Aritmetica.java
@@ -11,7 +11,7 @@ import com.udemy.excepciones.OperacionExcepcion;
  * @author user
  */
 public class Aritmetica {
-    public static int division(int numerador, int denominador) throws OperacionExcepcion {
+    public static int division(int numerador, int denominador) { //En el caso del RuntimeException no es necesario agregar la excepcion en la firma del metodo
         if (denominador == 0) {
             throw new OperacionExcepcion("Division por cero");
         }
